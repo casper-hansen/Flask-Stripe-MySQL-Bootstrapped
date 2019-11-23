9d8ec246d35593679e3331e1b1c59046f224a912
Casper Bøgeskov Hansen <casperbh.96@gmail.com>
2019-11-24 00:22:14 +0100

    Added all static files - now it works

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template
 import os
 
-template_dir = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..'))
-template_dir = os.path.join(template_dir, 'frontend')
-template_dir = os.path.join(template_dir, 'templates')
+base_dir = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..'))
+frontend_dir = os.path.join(base_dir, 'frontend')
+template_dir = os.path.join(frontend_dir, 'templates')
+static_dir = os.path.join(frontend_dir, 'static')
 
-static_folder='web/static'
-
-app = Flask(__name__, template_folder=template_dir)
+app = Flask(__name__, 
+            template_folder=template_dir,
+            static_url_path='', 
+            static_folder=static_dir)
 
 @app.route("/")
 def hello():
