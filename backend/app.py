1cca80fd88d48e1232e468eda58bf70374331d95
casperbh96 <casperbh.96@gmail.com>
2019-11-24 00:06:10 +0100

    Added some pages

--- backend/app.py
+++ backend/app.py
@@ -5,11 +5,18 @@ template_dir = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..'))
 template_dir = os.path.join(template_dir, 'frontend')
 template_dir = os.path.join(template_dir, 'templates')
 
+static_folder='web/static'
+
 app = Flask(__name__, template_folder=template_dir)
 
 @app.route("/")
 def hello():
-    return render_template('index.html')
+    return render_template('index.ejs')
+
+    
+@app.route("/login-page")
+def login():
+    return render_template('login-page.ejs')
 
 if __name__ == '__main__':
     app.debug = True
