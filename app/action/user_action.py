7880a1310865d758815b5991ce65a2745d91d4b8
casperbh96 <casperbh.96@gmail.com>
2019-12-21 12:33:26 +0100

    Made user service

--- app/action/user_action.py
+++ app/action/user_action.py
@@ -7,10 +7,72 @@ import sys
 import os
 from datetime import timedelta, datetime
 import traceback
+import time
+from flask_bcrypt import generate_password_hash, check_password_hash
+from sqlalchemy.exc import IntegrityError
 
 class UserAction():
-    def __init__(self, db, app, User, Notifications):
+    def __init__(self, db, app, User, Notifications, Stripe):
         self.User = User
         self.Notifications = Notifications
+        self.Stripe = Stripe
         self.db = db
         self.app = app
+
+    def signup(self, request):
+        try:
+            # Get data from AJAX request
+            data = request.get_json(force=True)
+            email = data['email']
+            password = data['password']
+
+            # Hash the password (store only the hash)
+            pw_hash = generate_password_hash(password, 10)
+
+            # Save user in database
+            new_user = self.User(email=email, password_hash=pw_hash)
+
+            # Take the row spot
+            self.db.session.add(new_user)
+            self.db.session.flush()
+
+            # Make notification
+            new_notification = self.Notifications(user_id=new_user.id,
+                                                  color = 'success',
+                                                  icon = 'check-circle',
+                                                  message_preview = 'You are signed up! Thanks for joining this service.',
+                                                  message = 'You have successfully signed up!')
+            self.db.session.add(new_notification)
+
+            # Commit changes
+            self.db.session.commit()
+
+            return json.dumps({'message':'/login_page'}), 200
+        except IntegrityError as ex:
+            self.db.session.rollback()
+            return json.dumps({'message':'Email already in use, tried logging in?'}), 403
+        except Exception as ex:
+            return json.dumps({'message':'Something went wrong'}), 401
+
+    def login(self, request):
+        try:
+            # Get data from AJAX request
+            data = request.get_json(force=True)
+            email = data['email']
+            password = data['password']
+
+            # Find user
+            user = self.User.query.filter_by(email=email).first()
+
+            # If user exists, check if email and password matches
+            if user != None:
+                check_pw = check_password_hash(user.password_hash, password)
+                if user.email == email and check_pw:
+                    login_user(user, remember=True)
+                    return json.dumps({'message':'/dashboard'}), 200
+                else:
+                    return json.dumps({'message':'User data incorrect'}), 401
+            else:
+                return json.dumps({'message':'Email not registered'}), 401
+        except Exception as ex:
+            return json.dumps({'message':'Unknown error, we apologize'}), 500
--- app/services/UserService/main_user.py
+++ app/services/UserService/main_user.py
@@ -1,17 +1,57 @@
 import sys
+import json
 import os
 import stripe
 from flask import request
-from flask_login import login_required, current_user
+
+from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user, current_user
 
 base_dir = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', '..'))
 sys.path.append(base_dir)
 
 # Import all the things
-from backend import app, db, User, Notifications, csrf
+from backend import app, db, User, Notifications, Stripe, csrf
 from action.user_action import UserAction
 
-action = UserAction(db, app, User, Notifications)
+action = UserAction(db, app, User, Notifications, Stripe)
+
+@app.route("/signup", methods=["POST"])
+def signup():
+    '''
+        Endpoint for signing up a user and saving in the database.
+
+        Parameters
+        ----------
+        request : JSON data
+            Has to contain the email and password for a user.
+
+        Returns (JSON)
+        -------
+        {
+            message : string 
+                Renders a URL to the login page on success. Gives error message otherwise.
+        }
+    '''
+    return action.signup(request)
+
+@app.route("/login", methods=["POST"])
+def login():
+    '''
+        Endpoint for logging in.
+
+        Parameters
+        ----------
+        request : JSON data
+            Has to contain the email and password for a user.
+
+        Returns (JSON)
+        -------
+        {
+            message : string 
+                Renders a URL to the dashboard on success. Gives error message otherwise.
+        }
+    '''
+    return action.login(request)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
