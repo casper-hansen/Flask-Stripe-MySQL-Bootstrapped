727fdd25921e58483f3ab7adc5c5d8f4f3069ee4
casperbh96 <casperbh.96@gmail.com>
2019-12-22 20:08:48 +0100

    Added login required

--- app/services/FrontendService/call_notifications_service.py
+++ app/services/FrontendService/call_notifications_service.py
@@ -1,10 +1,12 @@
 from flask import Flask, Blueprint, request
 from models.notifications import Notifications, db
+from flask_login import current_user, login_required
 import json, requests
 
 notification_api = Blueprint('notification_api', __name__)
 
 @notification_api.route("/notification_read", methods=["PUT"])
+@login_required
 def notification_read():
     data = request.get_json(force=True)
 
--- app/services/FrontendService/call_stripe_service.py
+++ app/services/FrontendService/call_stripe_service.py
@@ -1,11 +1,12 @@
 from flask import Flask, Blueprint, request
-from flask_login import current_user
+from flask_login import current_user, login_required
 from models.stripe import Stripe, db
 import json, requests
 
 stripe_api = Blueprint('stripe_api', __name__)
 
 @stripe_api.route("/setup_payment", methods=["POST"])
+@login_required
 def setup_payment():
     data = request.get_json(force=True)
     data['user_id'] = current_user.id
@@ -15,6 +16,7 @@ def setup_payment():
     return r.text, r.status_code
 
 @stripe_api.route("/cancel_subscription", methods=["PUT"])
+@login_required
 def cancel_subscription():
     data = request.get_json(force=True)
     data['user_id'] = current_user.id
@@ -24,6 +26,7 @@ def cancel_subscription():
     return r.text, r.status_code
 
 @stripe_api.route("/reactivate_subscription", methods=["PUT"])
+@login_required
 def reactivate_subscription():
     data = request.get_json(force=True)
     data['user_id'] = current_user.id
