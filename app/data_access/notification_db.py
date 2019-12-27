7fdf0d3f62901b1a33cadda36304fda74954a727
casperbh96 <casperbh.96@gmail.com>
2019-12-27 16:21:20 +0100

    Added stuff to notification db

--- app/data_access/notification_db.py
+++ app/data_access/notification_db.py
@@ -1,9 +1,25 @@
+from models.notifications import Notifications, db
 
 class NotificationAccess():
-
     def __init__(self):
-        pass
+        self.db = db
+        self.Notification = Notifications
 
-    def create_notification(self):
-        pass
+    def get_stripe(self, noti_id=None, get_all=False):
+        if noti_id != None:
+            noti_obj = self.Notification.query.filter_by(id=noti_id)
+
+        if get_all:
+            return noti_obj.all()
+        else:
+            return noti_obj.first()
     
+    def create_notification(self, notification_dict):
+        notification_row = self.Notification(**notification_dict)
+        self.db.session.add(notification_row)
+        self.db.session.commit()
+    
+    def update_stripe_by_dict(self, noti_id, noti_obj):
+        notification_row = self.Notification.query.filter_by(id=noti_id).first()
+        notification_row.update(**noti_obj)
+        self.db.session.commit()
